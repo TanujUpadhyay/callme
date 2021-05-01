85619d6334765fa65f15349ae82e915ef214eb9f
Tanuj <tanujupadhyay145.t.u@gmail.com>
2021-05-01 17:52:07 +0530

    adding socket io functions

--- index.js
+++ index.js
@@ -12,11 +12,30 @@ const io = require("socket.io")(server,{
 app.use(cors());
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8090;
 
 
 app.get("/",(req,res)=>{
     res.send("Server is running.");
 })
+
+
+
+io.on("connection",(socket)=>{
+    socket.emit("me",socket.id);
+
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("callended")
+    })
+
+    socket.on("calluser",({userTocall,signalData,from,name})=>{
+        io.to(userTocall).emit("calluser",{signal:signalData,from,name})
+    })
+
+    socket.on("answercall",(data)=>{
+        io.to(data.to).emit("callaccepted",data.signal)
+    })
+})
+
 
 server.listen(PORT,()=> console.log(`SERVER IS RUNNING ON PORT ${PORT}`))
